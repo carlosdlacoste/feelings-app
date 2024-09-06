0eba6dd74495d93ec7b34cf565617c64b7e37e0d
Carlos D'Lacoste <cdlacostesalazar@gmail.com>
2024-09-06 17:37:19 -0400

    Add nextui table to show csv data

--- src/components/feelingsAnalizer.jsx
+++ src/components/feelingsAnalizer.jsx
@@ -1,50 +1,74 @@
 "use client"
 import { useState, useEffect } from "react"
+import {
+    Table,
+    TableHeader,
+    TableBody,
+    TableColumn,
+    TableRow,
+    TableCell
+} from "@nextui-org/react";
 
 const FeelingsAnalizer = ({data}) => {
 
     const [modelResult, setModelResult] = useState([])
     const [maxLabel, setMaxLabel] = useState("")
 
-    useEffect(() => {
+    // useEffect(() => {
 
-        const apiRequest = async () => {
+    //     const apiRequest = async () => {
 
-                try {
-                    data.forEach(element => {
-                        fetch("https://api-inference.huggingface.co/models/finiteautomata/beto-sentiment-analysis", {
-                            method: "POST",
-                            headers: {
-                                "Authorization": "Bearer " + "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"
-                            },
-                            body: JSON.stringify({
-                                "text": element.text
-                            }),
-                        })
-                        .then(resp => resp.json())
-                        .then(result => {
-                            console.log(result)
-                            setModelResult(result)
-                        })
-                    });
-                    // return result
-                } catch (error) {
-                    console.log(error)
-                }
-        }
-        apiRequest()
-    }, [data]);
+    //             try {
+    //                 data.forEach(element => {
+    //                     fetch("https://api-inference.huggingface.co/models/finiteautomata/beto-sentiment-analysis", {
+    //                         method: "POST",
+    //                         headers: {
+    //                             "Authorization": "Bearer " + "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"
+    //                         },
+    //                         body: JSON.stringify({
+    //                             "text": element.text
+    //                         }),
+    //                     })
+    //                     .then(resp => resp.json())
+    //                     .then(result => {
+    //                         console.log(result)
+    //                         setModelResult(result)
+    //                     })
+    //                 });
+    //                 // return result
+    //             } catch (error) {
+    //                 console.log(error)
+    //             }
+    //     }
+    //     apiRequest()
+    // }, [data]);
 
-    useEffect(() => {
-        if(modelResult.length > 0){
-            const maxObjeto = modelResult.reduce((max, item) => item.score > max.score ? item : max, modelResult[0])
-            setMaxLabel(maxObjeto.label)
-        }
-    }, [modelResult])
+    // useEffect(() => {
+    //     if(modelResult.length > 0){
+    //         const maxObjeto = modelResult.reduce((max, item) => item.score > max.score ? item : max, modelResult[0])
+    //         setMaxLabel(maxObjeto.label)
+    //     }
+    // }, [modelResult])
 
     return(
         <>
-        
+            <Table aria-label="Example static collection table">
+                <TableHeader>
+                    <TableColumn>ID</TableColumn>
+                    <TableColumn>Message</TableColumn>
+                    <TableColumn>Sentiment</TableColumn>
+                </TableHeader>
+                <TableBody>
+                    {data.map((item, index) => (
+                        <TableRow key={index}>
+                        <TableCell>{index}</TableCell>
+                        <TableCell>{item.text}</TableCell>
+                        <TableCell>N/A</TableCell>
+                        </TableRow>
+
+                    ))}
+                </TableBody>
+            </Table>
         </>
     )
 }
